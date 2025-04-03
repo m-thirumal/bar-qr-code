3bae21416910a18af29a4cf26f723502b1866b35
Thirumal <m.thirumal@hotmail.com>
2025-04-03 22:04:50 +0530

    first commit

---
+++ src/main/java/com/thirumal/BarQrCodeApplication.java
@@ -0,0 +1,13 @@
+package com.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarQrCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarQrCodeApplication.class, args);
+	}
+
+}
